765dcd7707da59011235c98ed8662501c810fb0a
dawe <dawedawe@posteo.de>
2024-12-03 23:45:42 +0100

    day 01 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 const INPUT: &str = "day01input.txt";
 
@@ -16,4 +16,23 @@ pub fn part1() {
     let pairs = left.into_iter().zip(right);
     let s: i32 = pairs.map(|(l, r)| (l - r).abs()).sum();
     println!("{s}")
+}
+
+pub fn part2() {
+    let input = fs::read_to_string(INPUT).expect("read_to_string failed");
+    let mut left: Vec<i32> = vec![];
+    let mut right: Vec<i32> = vec![];
+    input.lines().for_each(|line| {
+        let parts: Vec<_> = line.split_whitespace().collect();
+        left.push(parts[0].parse().unwrap());
+        right.push(parts[1].parse().unwrap());
+    });
+    let mut m: HashMap<i32, i32> = HashMap::new();
+    right.iter().for_each(|r| {
+        let c = m.get(r).unwrap_or(&0);
+        m.insert(*r, c + 1);
+    });
+
+    let s: i32 = left.iter().map(|l| l * m.get(l).unwrap_or(&0)).sum();
+    println!("{}", s)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use adventofcode2024rust::days;
 
 fn main() {
-    days::day01::part1();
+    days::day01::part2();
 }
