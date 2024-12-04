24213cc4bc9673404e6c638d91714b90716ac54c
dawe <dawedawe@posteo.de>
2024-12-05 00:26:07 +0100

    day 02 part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -28,3 +28,29 @@ pub fn part1() {
         .count();
     println!("{safe_reports}");
 }
+
+pub fn part2() {
+    let input = fs::read_to_string(INPUT).expect("read_to_string failed");
+    let safe_reports = input
+        .lines()
+        .filter(|line| {
+            let numbers = line
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+            if is_safe(&numbers) {
+                true
+            } else {
+                for i in 0..numbers.len() {
+                    let mut numbers2 = numbers.clone();
+                    numbers2.remove(i);
+                    if is_safe(&numbers2) {
+                        return true;
+                    }
+                }
+                false
+            }
+        })
+        .count();
+    println!("{safe_reports}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use adventofcode2024rust::days;
 
 fn main() {
-    days::day02::part1();
+    days::day02::part2();
 }
